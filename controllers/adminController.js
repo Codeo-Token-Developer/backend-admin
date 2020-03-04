e3184e8239623787127c5f44f48c2272e13444a1
laskar-ksatria <dock_fest@yahoo.co.id>
2020-03-04 09:56:02 +0700

    tambah dikit

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,7 @@
 const Admin = require('../models/admin');
 const User = require('../models/user');
 const Account = require('../models/account');
+const bankAccount = require('../models/bankAccount');
 const { checkPass } = require('../helpers/hashPassword');
 const { generateToken } = require('../helpers/jwt');
 
@@ -76,6 +77,22 @@ class AdminController {
             })
             .catch(next);
     };
+
+    static readAllVerifiedUser(req,res,next) {
+        User.find({verification: true})
+            .then(function (users) {
+                res.status(200).json({users, status: 200})
+            })
+            .catch(next);
+    };
+
+    static readAllBankAccount(req,res,next) {
+        bankAccount.find({}).populate('user')
+            .then(function (banks) {
+                res.status(200).json({banks, status: 200})
+            })
+            .catch(next)
+    }
 
 };
 
