4139b9d00e82e9aa7de51d1bcd3624ec35952ad0
thomasfebrianseiei <thomas-f-seiei@hotmail.com>
2020-03-09 11:23:37 +0700

    baru

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,17 +1,54 @@
-const express = require('express');
+const express = require("express");
 const Router = express.Router();
-const { AdminAuthorization, Authentication } = require('../middlewares/validator');
-const adminController = require('../controllers/adminController');
+const {
+  AdminAuthorization,
+  Authentication
+} = require("../middlewares/validator");
+const adminController = require("../controllers/adminController");
 
-Router.get('/', Authentication, AdminAuthorization, adminController.readAllAdmin);
-Router.get('/users', Authentication, AdminAuthorization, adminController.readAllUsers);
-Router.get('/accounts', Authentication, AdminAuthorization, adminController.readAllAccounts);
-Router.post('/', Authentication, AdminAuthorization, adminController.create);
-Router.post('/login', adminController.login);
-Router.delete('/', Authentication, AdminAuthorization, adminController.deleteUser);
-Router.patch('/', Authentication, AdminAuthorization, adminController.kycApproved);
-Router.get('/activedUser', Authentication, AdminAuthorization, adminController.readAllTotalActiveUser);
-Router.patch('/fee', Authentication, AdminAuthorization, adminController.updateFee);
-
+Router.get(
+  "/",
+  Authentication,
+  AdminAuthorization,
+  adminController.readAllAdmin
+);
+Router.get(
+  "/users",
+  Authentication,
+  AdminAuthorization,
+  adminController.readAllUsers
+);
+Router.get(
+  "/accounts",
+  Authentication,
+  AdminAuthorization,
+  adminController.readAllAccounts
+);
+Router.post("/", Authentication, AdminAuthorization, adminController.create);
+Router.post("/login", adminController.login);
+// Router.delete(
+//   "/",
+//   Authentication,
+//   AdminAuthorization,
+//   adminController.deleteUser
+// );
+Router.patch(
+  "/",
+  Authentication,
+  AdminAuthorization,
+  adminController.kycApproved
+);
+Router.get(
+  "/activedUser",
+  Authentication,
+  AdminAuthorization,
+  adminController.readAllTotalActiveUser
+);
+Router.patch(
+  "/fee",
+  Authentication,
+  AdminAuthorization,
+  adminController.updateFee
+);
 
 module.exports = Router;
